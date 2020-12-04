6775befd64625c2eee509abd23c490f9086e18d6
IdrissaD <73114156+IdrissaD@users.noreply.github.com>
2020-12-04 11:28:44 +0100

    Use of choropleth plugin
    Replacement of "hard" code for the scale of colors by the adaptable functions of leaflet-choropleth plugin

--- choromap.js
+++ choromap.js
@@ -1,153 +1,94 @@
 var osm_layer = L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 
-	attribution: '<a href = "https://areskidrissa.cc" target = "_blank">areskidrissa.cc</a> Map data &copy; <a href="https://www.openstreetmap.org/" target = "_blank">OpenStreetMap</a> contributors, ' +
-
-	'<a href="https://creativecommons.org/licenses/by-sa/2.0/" target = "_blank">CC-BY-SA</a>'
+	attribution: '<a href = "https://github.com/IdrissaD" target = "_blank">Idrissa Djepa</a> | Map data: Idrissa Djepa and <a href="https://www.openstreetmap.org/" target = "_blank">OpenStreetMap</a> contributors, ' +
+    '<a href="https://creativecommons.org/licenses/by-sa/2.0/" target = "_blank">CC-BY-SA</a>'
 });
 
 var choromap = L.map('choroid').setView([45.757523270000576, 4.831581115722656], 6);
 
-
-
+// Style the dpts, and hide those that have a tot_dist = 0
 function style_dpts(feature) {
-            if (feature.properties['tot_dist'] == 0) {
-                return {
-                opacity: 0,
-                color: 'rgba(35,35,35,1.0)',
-                dashArray: '',
-                lineCap: 'butt',
-                lineJoin: 'miter',
-                weight: 1.0, 
-                fill: true,
-                fillOpacity: 0,
-                fillColor: 'rgba(255,255,255,1.0)',
-                interactive: true,
-            }
-            }
-            if (feature.properties['tot_dist'] >= 0.000000 && feature.properties['tot_dist'] <= 50.000000 ) {
-                return {
-                opacity: 0,
-                color: 'rgba(35,35,35,1.0)',
-                dashArray: '',
-                lineCap: 'butt',
-                lineJoin: 'miter',
-                weight: 1.0, 
-                fill: true,
-                fillOpacity: 0.65,
-                fillColor: 'rgba(247,252,245,1.0)',
-                interactive: true,
-            }
-            }
-            if (feature.properties['tot_dist'] >= 50.000000 && feature.properties['tot_dist'] <= 100.000000 ) {
-                return {
-                opacity: 0,
-                color: 'rgba(35,35,35,1.0)',
-                dashArray: '',
-                lineCap: 'butt',
-                lineJoin: 'miter',
-                weight: 1.0, 
-                fill: true,
-                fillOpacity: 0.65,
-                fillColor: 'rgba(213,239,207,1.0)',
-                interactive: true,
-            }
-            }
-            if (feature.properties['tot_dist'] >= 100.000000 && feature.properties['tot_dist'] <= 150.000000 ) {
-                return {
-                opacity: 0,
-                color: 'rgba(35,35,35,1.0)',
-                dashArray: '',
-                lineCap: 'butt',
-                lineJoin: 'miter',
-                weight: 1.0, 
-                fill: true,
-                fillOpacity: 0.65,
-                fillColor: 'rgba(158,216,152,1.0)',
-                interactive: true,
-            }
-            }
-            if (feature.properties['tot_dist'] >= 150.000000 && feature.properties['tot_dist'] <= 200.000000 ) {
-                return {
-                opacity: 0,
-                color: 'rgba(35,35,35,1.0)',
-                dashArray: '',
-                lineCap: 'butt',
-                lineJoin: 'miter',
-                weight: 1.0, 
-                fill: true,
-                fillOpacity: 0.65,
-                fillColor: 'rgba(84,181,103,1.0)',
-                interactive: true,
-            }
-            }
-            if (feature.properties['tot_dist'] >= 200.000000 && feature.properties['tot_dist'] <= 250.000000 ) {
-                return {
-                opacity: 0,
-                color: 'rgba(35,35,35,1.0)',
-                dashArray: '',
-                lineCap: 'butt',
-                lineJoin: 'miter',
-                weight: 1.0, 
-                fill: true,
-                fillOpacity: 0.65,
-                fillColor: 'rgba(29,134,65,1.0)',
-                interactive: true,
-            }
-            }
-            if (feature.properties['tot_dist'] >= 250.000000) {
-                return {
-                opacity: 0,
-                color: 'rgba(35,35,35,1.0)',
-                dashArray: '',
-                lineCap: 'butt',
-                lineJoin: 'miter',
-                weight: 1.0, 
-                fill: true,
-                fillOpacity: 0.65,
-                fillColor: 'rgba(0,68,27,1.0)',
-                interactive: true,
-            }
-            }
+    if (feature.properties.tot_dist == 0) {
+        return {
+            opacity: 0,
+            fillOpacity: 0    
+        }
+    } else {
+        return {
+            opacity: 0,
+            fillOpacity: 0.65
+        }
+    }
+    
 };
 
-function getColor(d) {
-    return d > 250  ? 'rgba(0,68,27,1.0)' :
-           d > 200  ? 'rgba(29,134,65,1.0)' :
-           d > 150  ? 'rgba(84,181,103,1.0)' :
-           d > 100   ? 'rgba(158,216,152,1.0)' :
-           d > 50   ? 'rgba(213,239,207,1.0)' :
-           d > 0   ? 'rgba(247,252,245,1.0)' :
-           'rgba(255,255,255,1.0)';
-}
+// Define what to do with each feature:
+// bind a different popup content if tot_dist = 0 or not
+// referr to mouse interactions functions
+function onEachFeature(feature, layer) {
+    if (feature.properties.tot_dist){
+        layer.bindPopup('<p><b>' + feature.properties.nom + '</b></p><p>' + parseFloat(feature.properties.tot_dist).toFixed(0) + ' km</p><p>Nombre de voyages : ' + feature.properties.nb_trips)
+        } else {
+            layer.bindPopup('<p><b>' + feature.properties.nom + '</b></p><p>Pas de voyage')
+        };
+        layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight
+    });
+};
 
-var dpts_layer = L.geoJson(dpts, {
-        onEachFeature(feature, layer)
-        {layer.bindPopup('<p><b>' + feature.properties.nom + '</b></p><p>' + parseFloat(feature.properties.tot_dist).toFixed(0) + ' km</p><p>Nombre de voyages : ' + feature.properties.nb_trips)},
-        style: style_dpts
+// Define the style changements when hovering a dpt
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 2.5,
+        color: 'green',
+        opacity: 1
     });
 
-
-
-var legend = L.control({position: 'bottomright'});
-
-legend.onAdd = function (map) {
-
-    var div = L.DomUtil.create('div', 'info legend'),
-        grades = [0, 50, 100, 150, 200, 250],
-        labels = [];
-
-    div.innerHTML = '<p><b>Nombre de kilomètres<br>parcourus dans<br> le département</b></p>'
-
-    // loop through our density intervals and generate a label with a colored square for each interval
-    for (var i = 0; i < grades.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    // Necessary for other browsers than Firefox, which don't support bringToFront
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
     }
-
-    return div;
 };
 
+// Reset the style when hovering is finished
+function resetHighlight(e) {
+    dpts_layer.resetStyle(e.target);
+};
+
+// Build the choropleth map with choropleth plugin
+var dpts_layer = L.choropleth(dpts, {
+    valueProperty: 'tot_dist',
+    scale: ['white', 'green'],
+    steps: 6,
+    mode: 'e',
+    style: style_dpts,
+    onEachFeature: onEachFeature
+});
+
+// Build the legend
+var legend = L.control({ position: 'bottomright' })
+  legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend')
+    var limits = dpts_layer.options.limits
+    var colors = dpts_layer.options.colors
+    var labels = []
+
+    // Add min & max
+    div.innerHTML = '<div class="text"><p><b>Nombre de kilomètres parcourus<br>dans le département</b></p></div><div class="labels"><div class="min">' + limits[0] + '</div> \
+            <div class="max">' + parseFloat(limits[limits.length - 1]).toFixed(0) + '</div></div>'
+
+    limits.forEach(function (limit, index) {
+      labels.push('<li style="background-color: ' + colors[index] + '"></li>')
+    })
+
+    div.innerHTML += '<ul>' + labels.join('') + '</ul>'
+    return div
+};
+
+// Add the layers and legend to the map
 dpts_layer.addTo(choromap);
 legend.addTo(choromap);
 osm_layer.addTo(choromap);
